a85c403a2a52f14bece7e1d23aec068da6c023f0
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2023-12-14 15:18:47 +0300

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Егор Аблогин on 14.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
